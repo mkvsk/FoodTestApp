12d64b350233bb1c611f1c5d72d253bf3e5579f3
mkvsk <tanysha6@me.com>
2023-06-09 18:51:14 +0200

    Add: date formatter

---
+++ app/src/main/java/com/example/foodtestapp/ui/util/FormatUtil.kt
@@ -0,0 +1,27 @@
+package com.example.foodtestapp.ui.util
+
+import java.time.LocalDateTime
+
+object FormatUtil {
+    fun getCurrentDateFormat(): String {
+        val current = LocalDateTime.now()
+        return "${current.dayOfMonth} ${getMonthName(current.monthValue)}, ${current.year}"
+    }
+
+    private fun getMonthName(monthValue: Int): String {
+        return when (monthValue) {
+            1 -> "Января"
+            2 -> "Февраля"
+            3 -> "Марта"
+            4 -> "Апреля"
+            5 -> "Мая"
+            6 -> "Июня"
+            7 -> "Июля"
+            8 -> "Августа"
+            9 -> "Сентября"
+            10 -> "Октября"
+            11 -> "Ноября"
+            else -> "Декабря"
+        }
+    }
+}
